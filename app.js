b374efa63b9d5dd2d896ae8b2a5ba2c97bf32165
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2021-07-16 00:42:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const resultDiv = document.querySelector('.result');
+const wordEle = document.querySelector('.word');
+const phonetics = document.querySelector('.phonetics');
+const audio = document.querySelector('audio');
+const wordMeaning = document.querySelector('.word-defination');
+const synonyms = document.querySelector('.synonyms');
+
+
+const url = ('https://api.dictionaryapi.dev/api/v2/entries/en_US/')
+
+// Make a request to the api
+const handle = async (e) => {
+    if (e.keyCode === 13) {
+        const word = e.target.value;
+        const result = await fetch(url + word);
+        if (!result.ok) {
+            alert("😬 Oops!! We didn't get any match in our database. Please check your word and try again...")
+        }
+        const data = await result.json();
+        resultDiv.style.display = "block";
+        wordEle.innerText = data[0].word;
+        phonetics.innerText = data[0].phonetics[0].text;
+        audio.src = data[0].phonetics[0].audio;
+        wordMeaning.innerText = data[0].meanings[0].definitions[0].definition;
+        const synonymsArray = data[0].meanings[0].definitions[0].synonyms;
+        if (synonymsArray) {
+            let synonymsData = "";
+            for (let i = 0; i < synonymsArray.length; i++) {
+                synonymsData += `<p class="synonym-fields">${synonymsArray[i]}<p>`;
+            }
+            synonyms.innerHTML = synonymsData;
+        }
+        else {
+            synonyms.innerHTML = '<p class= "synonym-fields">No synonyms found</p>';
+        }
+    }
+};
